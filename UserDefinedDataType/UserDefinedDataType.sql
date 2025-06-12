c8edb87c94a1e4f8d0c8c1b238592e91953c671f
imrankhanv11 <146421206+imrankhanv11@users.noreply.github.com>
2025-06-12 18:13:30 +0530

    Add files via upload

--- UserDefinedDataType/UserDefinedDataType.sql
+++ UserDefinedDataType/UserDefinedDataType.sql
@@ -1,14 +1,14 @@
---Create Scalar UDT
+--Create scalar UDT
 CREATE TYPE PhoneNumber FROM VARCHAR(15) NOT NULL;
 
---Use UDT in a Table
+--use UDT in a Table
 CREATE TABLE Contacts (
     ContactID INT PRIMARY KEY,
     FullName VARCHAR(100),
     Mobile PhoneNumber -- using scalar UDT
 );
 
---Insert Data Using UDT
+--Insert data using UDT
 INSERT INTO Contacts VALUES (1, 'Alice', '1234567890');
 
 --Declare and Use a Variable of UDT Type
@@ -16,19 +16,19 @@ DECLARE @MyPhone PhoneNumber;
 SET @MyPhone = '9876543210';
 PRINT 'My Phone: ' + @MyPhone;
 
---Use UDT in a Stored Procedure
+--Use UDT in a Stored procedure
 CREATE PROCEDURE AddContact
-    @FullName VARCHAR(100),
+    @Name VARCHAR(100),
     @Mobile PhoneNumber
 AS
 BEGIN
-    INSERT INTO Contacts (FullName, Mobile) VALUES (@FullName, @Mobile);
+    INSERT INTO Contacts (FullName, Mobile) VALUES (@Name, @Mobile);
 END;
 
--- Call the procedure
+-- Calling
 EXEC AddContact 'Bob', '7778889999';
 
---Use UDT in a Scalar Function
+--Use UDT in Scalar function
 CREATE FUNCTION FormatPhone (@Mobile PhoneNumber)
 RETURNS VARCHAR(20)
 AS
@@ -41,10 +41,10 @@ SELECT FullName, dbo.FormatPhone(Mobile) AS FormattedMobile FROM Contacts;
 
 --unsupported features ( this will fails )
 --can't create constraints inside UDT:
--- CREATE TYPE EmailType FROM VARCHAR(50) CHECK (EmailType LIKE '%@%');Not allowed
+-- CREATE TYPE EmailType FROM VARCHAR(50) CHECK (EmailType LIKE '%@%'); ---------Not allowed
 
 --can't ALTER a UDT
--- ALTER TYPE PhoneNumber FROM VARCHAR(20); --Not allowed
+-- ALTER TYPE PhoneNumber FROM VARCHAR(20); -----------Not allowed
 
 --Drop UDT (only after removing all dependencies)
 --Clean up (drop procedure, function, table first)
@@ -57,21 +57,21 @@ DROP TYPE PhoneNumber;
 --Create Scalar UDT (used inside the table type)
 CREATE TYPE PhoneNumber FROM VARCHAR(15) NOT NULL;
 
---Create User-Defined Table Type (UDTT)
+--Cteate User-Defined Table Type (UDTT)
 CREATE TYPE EmployeeTableType AS TABLE (
     EmpID INT PRIMARY KEY,
     Name VARCHAR(100),
-    Mobile PhoneNumber -- using scalar UDT here
+    Mobile PhoneNumber -------------- using scalar UDT here
 );
 
---Create target table
+--Create table
 CREATE TABLE Employees (
     EmpID INT PRIMARY KEY,
     Name VARCHAR(100),
     Mobile PhoneNumber
 );
 
---Create a stored procedure that accepts the UDTT
+--Create a stored procedure correct--
 CREATE PROCEDURE InsertEmployees
     @EmpData EmployeeTableType READONLY
 AS
@@ -108,3 +108,67 @@ DROP PROCEDURE InsertEmployees;
 DROP TABLE Employees;
 DROP TYPE EmployeeTableType;
 DROP TYPE PhoneNumber;
+
+------------------------------------------------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE SaleProducts
+	@customerId INT,
+	@saleDate DATE,
+	@userId INT,
+	@warehouseId INT,
+	@salesdetails SALESTABLE READONLY
+AS	
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @saleId INT;
+	DECLARE @totalprice DECIMAL(16,5);
+
+	BEGIN TRY
+
+		SELECT @totalprice = SUM(QUANTITY * PRICE) FROM @salesdetails;
+
+		BEGIN TRANSACTION;
+
+			INSERT INTO Sales (customer_id, sale_date, total_price, user_id)
+			VALUES (@customerId, @saleDate, @totalprice, @userId);
+
+			SET @saleId = SCOPE_IDENTITY();
+
+			INSERT INTO SalesDetails (sale_id, product_id, quantity, price)
+			SELECT @saleId, ProudctID, QUANTITY, QUANTITY*PRICE AS PRICE FROM @salesdetails;
+
+			DECLARE @product_id INT, @qty INT, @price DECIMAL(16,5);
+
+			DECLARE product_cursor CURSOR FOR
+			SELECT ProudctID, QUANTITY, PRICE FROM @salesdetails;
+
+			OPEN product_cursor;
+			FETCH NEXT FROM product_cursor INTO @product_id, @qty, @price;
+
+			WHILE @@FETCH_STATUS = 0
+			BEGIN
+				UPDATE ProductWarehouseStock
+				SET quantity_in_stock = quantity_in_stock - @qty
+				WHERE product_id = @product_id AND warehouse_id = @warehouseId;
+
+				INSERT INTO Inventory_Log (product_id, action, quantity, action_date, reference_id, user_id, warehouse_id)
+				VALUES (@product_id, 'SALE', @qty, @saleDate, @saleId, @userId, @warehouseId);
+
+				FETCH NEXT FROM product_cursor INTO @product_id, @qty, @price;
+			END
+
+			CLOSE product_cursor;
+			DEALLOCATE product_cursor;
+
+		COMMIT TRANSACTION;
+
+		PRINT 'Sale completed. Sale ID: ' + CAST(@saleId AS VARCHAR);
+
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		PRINT 'Error: ' + ERROR_MESSAGE();
+	END CATCH
+END;
+GO
